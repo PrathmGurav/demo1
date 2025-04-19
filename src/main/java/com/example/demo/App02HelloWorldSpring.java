006255fea9523f04f898ae6be86e33815cca2079
Prathm-Gurav <PrathmeshGurav22@gmail.com>
2025-04-19 09:35:42 +0530

    n

--- src/main/java/com/example/demo/App02HelloWorldSpring.java
+++ src/main/java/com/example/demo/App02HelloWorldSpring.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.example.demo.game.GameRunner;
 import com.example.demo.game.MarioGame;
@@ -11,11 +12,22 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 
-//		var game = new MarioGame();
-		var game = new SuperContra();
-
-		var gameRunner = new GameRunner(game);
-		gameRunner.run();
+		//1 Launch a Spring context
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+				
+		//2 configure things that we want spring to manage - @Configuration
+//		System.out.println(context.getBean("name"));
+//		
+//		System.out.println(context.getBean("MyName"));
+		
+//		System.out.println(context.getBean("person"));
+		
+		//Calling Bean by Type
+//		System.out.println(context.getBean(Address.class));
+		
+//		System.out.println(context.getBean("person2ByMethodCall"));
+		
+		System.out.println(context.getBean("person3ByParameters"));
 	}
 
 }
