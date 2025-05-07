13ec761c8639bb5942e163a551a8b7de00d60ff7
a <a@a-Vostro-3670>
2025-05-07 14:18:07 +0530

    hb

--- src/main/java/com/example/demo/BeanScope/SimpleLauncherApplication.java
+++ src/main/java/com/example/demo/BeanScope/SimpleLauncherApplication.java
@@ -1,5 +1,4 @@
 package com.example.demo.BeanScope;
-
 import java.util.Arrays;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -16,31 +15,27 @@ import com.example.demo.game.GammingConsole;
 @Component
 class NormalClass{
 	
-	
 }
 
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 class PrototypeClass{
 	
-	
 }
-
 
 @ComponentScan
 public class SimpleLauncherApplication {
-
-
-
 	public static void main(String[] args) {
 		try (// 1 Launch a Spring context
 				var context = new AnnotationConfigApplicationContext(SimpleLauncherApplication.class);) {
+				
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+//			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			
-			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-	
-
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
 		}
-
 	}
-
 }
